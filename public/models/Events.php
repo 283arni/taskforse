06283293a610918f6886dcf55f8dbc1a424378a8
Aleksandr Arnautov <283arni@gmail.com>
2026-02-04 17:09:53 +0300

    dz-6

---
+++ public/models/Events.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "events".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $task_id
+ * @property string $type
+ * @property string $dt_add
+ * @property int $is_read
+ *
+ * @property Tasks $task
+ * @property Users $user
+ */
+class Events extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'events';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['is_read'], 'default', 'value' => 0],
+            [['user_id', 'task_id', 'type'], 'required'],
+            [['user_id', 'task_id', 'is_read'], 'integer'],
+            [['dt_add'], 'safe'],
+            [['type'], 'string', 'max' => 12],
+            [['task_id'], 'exist', 'skipOnError' => true, 'targetClass' => Tasks::class, 'targetAttribute' => ['task_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::class, 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'task_id' => 'Task ID',
+            'type' => 'Type',
+            'dt_add' => 'Dt Add',
+            'is_read' => 'Is Read',
+        ];
+    }
+
+    /**
+     * Gets query for [[Task]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTask()
+    {
+        return $this->hasOne(Tasks::class, ['id' => 'task_id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(Users::class, ['id' => 'user_id']);
+    }
+
+}
